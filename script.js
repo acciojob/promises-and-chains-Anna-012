8e5680491c95834f96a64de0931cbfdb82ee55bd
Anna shabbagh <afreenshabbagh@gmail.com>
2024-11-21 14:40:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
 //your JS code here. If required.
-let firstpromise = new Promise((resolve, reject)=>{
-	setTimeout(()=>{
-		if{
-		resolve alert(`Welcome ${name}. You can vote.`)
-	},4000)
-}else{
-	reject alert(`Oh sorry ${name} You aren't old enough.`)
-}
+document.querySelector('form').addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  // Your promise should be created here
+	let age = document.getElementById('age').value;
+let name = document.getElementById('name').value;
+
+	age = Number(age);
+
+	let firstpromise = new Promise((resolve, reject)=>{
+  setTimeout(()=>{
+    if(age >= 18){
+      resolve (`Welcome ${name}. You can vote.`)
+    } else {
+      reject (`Oh sorry ${name} You aren't old enough.`)
+    }
+  }, 4000)
+});
 
 firstpromise.then((message)=>{
-     console.log(message);
-})
-
-})
+  console.log(message);
+}).catch((message)=>{
+  console.log(message);
+});
+});
