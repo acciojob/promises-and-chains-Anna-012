feddfdcad57c91e6dc1a38f40182d0c59846a717
Anna shabbagh <afreenshabbagh@gmail.com>
2024-11-21 14:33:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+let firstpromise = new Promise((resolve, reject)=>{
+	setTimeout(()=>{
+		if{
+		resolve alert(`Welcome ${name}. You can vote.`)
+	},4000)
+}else{
+	reject alert(`Oh sorry ${name} You aren't old enough.`)
+}
+
+firstpromise.then((message)=>{
+     console.log(message);
+})
+
+})
